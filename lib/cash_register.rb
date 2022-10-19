49dce4171aa913686ad940d83ab03469c8c1672a
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-10-19 14:08:42 +0300

    completed assignment

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,29 +1,20 @@
 class CashRegister
-    attr_reader :discount
-    attr_accessor :total
+    attr_accessor :total, :discount, :items, :amount
     def initialize(discount = 0)
         @total = 0
         @discount = discount
         @items = []
         @last_transaction = 0
     end
-    def add_item(title,price,quantity = 1)
+    def add_item(item,price,quantity = 1)
         @total += price * quantity
         quantity.times do
-            @items << title
+            @items << item
         end
         @last_transaction = price * quantity
     end
     def apply_discount
-        if @discount > 0
-            @total -= @total * @discount / 100
-            "After the discount, the total comes to $#{@total}."
-        else
-            "There is no discount to apply."
-        end
-    end
-    def items
-        @items
+        @discount > 0 ? "After the discount, the total comes to $#{@total -= @total * @discount / 100}." : "There is no discount to apply."
     end
     def void_last_transaction
         @total -= @last_transaction
